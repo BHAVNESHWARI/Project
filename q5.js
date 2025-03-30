dff28dd45937b3da47ebb618e47b1f138e3be20b
bhavneshwari dadheech <bhavneshwaridadhich@gmail.com>
2025-03-30 17:31:23 +0530

    Add files via upload

---
+++ q5.js
@@ -0,0 +1,12 @@
+function charAtEvenPos(N,str) {
+    //write code here
+    
+    // for(let i=0;i<=str.length;i++){
+    
+    for(let i=0;i<N;i+=2){
+      
+    
+    console.log(str[i])
+    
+    }
+}
