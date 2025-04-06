f7acacc2963c3cf56a5bafb52039687322093392
bhavneshwari dadheech <bhavneshwaridadhich@gmail.com>
2025-04-06 19:46:54 +0530

    Add files via upload

---
+++ Question2.js
@@ -0,0 +1,14 @@
+let product =[
+    {
+        name:"Laptop",category:"Electronics",Price:999.99,stock:15
+    },
+    {name:"Desk Chair",category:"Furniture",Price:149.50,stock:3},
+    {name:"coffee Maker",category:"Appliances",price:59.99,stock:8},
+    {
+        name:"Monitor",category:"Electronics",price:199.99,stock:12
+    },
+    {name:"Desk",category:"Furniture",price:249.50,stock:4},
+    {name:"Blender",category:"Appliances",price:39.99,stock:2}
+]
+// console.log(`Most stocked category: ${product}`);
+console.log(product)
